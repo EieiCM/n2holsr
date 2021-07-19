b2564398d225d81c3a0cf5a81cd1f2fb4e8da236
EieiCM <33316654+EieiCM@users.noreply.github.com>
2021-07-19 10:34:58 +0700

    Add MAX_N2HTTL to 3 for ttl of tc

--- src/olsr_protocol.h
+++ src/olsr_protocol.h
@@ -178,6 +178,7 @@ static INLINE const char * linkTypeToString(int type) {
  */
 #define MAXJITTER             HELLO_INTERVAL / 4
 #define MAX_TTL               0xff
+#define MAX_N2HTTL            0x03
 
 /*
  *Sequence numbering
